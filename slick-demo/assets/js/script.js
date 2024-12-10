ed08124da664a9adac0de6f4aa8c8c9b1a3b53b6
macmcclure1 <157757354+macmcclure1@users.noreply.github.com>
2024-12-10 03:01:23 +0000

    Fixing a new Space X Revision assignment

--- slick-demo/assets/js/script.js
+++ slick-demo/assets/js/script.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
 
-    $('.fade').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      slide: 'div',
-      cssEase: 'linear',
-      autoplay: true,
-      autoplaySpeed: 2000
-    });
-
-    $('.multiple-items').slick({
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 4
-    });
+ 
+  $('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    slide: 'div',
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+   
 });
